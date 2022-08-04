7bcee37573b1c21a9edba98b5fb794ef9b06569d
Alex Via <alexandervia@gmail.com>
2022-08-03 21:30:56 -0500

    Adds RESTful dynamic create, update, and destroy routes for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,10 +1,38 @@
 class ActorsController < ApplicationController
 
   def index
-    render json: Actor.all
+    actor = Actor.all
+    render json: actor.as_json
   end
 
   def show
-    render json: Actor.find_by(id: params[:id])
+    actor = Actor.find_by(id: params[:id])
+    render json: actor.as_json
   end
+
+  def create
+    actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for]  
+    )
+    actor.save
+    render json: actor.as_json
+  end
+
+  def update
+    actor = Actor.find_by(id: params[:id])
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.save
+    render json: actor.as_json
+  end
+
+  def delete
+    actor = Actor.find_by(id: params[:id])
+    actor.destroy
+    render json: {message: "Actor has been deleted"}
+  end
+
 end
